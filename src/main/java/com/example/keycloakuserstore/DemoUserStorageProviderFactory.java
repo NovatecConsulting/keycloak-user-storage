6231b5dc1341b4e9a3e1370df1dde667656a4913
Dorian Weidler <dorian.weidler@novatec-gmbh.de>
2020-01-16 14:36:58 +0100

    DB works

--- src/main/java/com/example/keycloakuserstore/DemoUserStorageProviderFactory.java
+++ src/main/java/com/example/keycloakuserstore/DemoUserStorageProviderFactory.java
@@ -45,10 +45,11 @@ public class DemoUserStorageProviderFactory implements UserStorageProviderFactor
         properties.put("hibernate.connection.url", "jdbc:mysql://localhost/jee_test");
         properties.put("hibernate.connection.username", "root");
         properties.put("hibernate.connection.password", "root");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
+//        properties.put("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
         properties.put("hibernate.show-sql", "true");
         properties.put("hibernate.archive.autodetection", "class, hbm");
-        properties.put("hibernate.hbm2ddl.auto", "create");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.connection.autocommit", "true");
 
         /*entityManagerFactory = Persistence.createEntityManagerFactory("h2userstorage");*/
         entityManagerFactory = new HibernatePersistenceProvider().createContainerEntityManagerFactory(getPersistenceUnitInfo("h2userstorage"), properties);
@@ -62,43 +63,36 @@ public class DemoUserStorageProviderFactory implements UserStorageProviderFactor
         return new PersistenceUnitInfo() {
             @Override
             public String getPersistenceUnitName() {
-                System.err.println("public String getPersistenceUnitName()");
                 return name;
             }
 
             @Override
             public String getPersistenceProviderClassName() {
-                System.err.println("public String getPersistenceProviderClassName()");
                 return "org.hibernate.jpa.HibernatePersistenceProvider";
             }
 
             @Override
             public PersistenceUnitTransactionType getTransactionType() {
-                System.err.println("public PersistenceUnitTransactionType getTransactionType()");
                 return PersistenceUnitTransactionType.RESOURCE_LOCAL;
             }
 
             @Override
             public DataSource getJtaDataSource() {
-                System.err.println("public DataSource getJtaDataSource()");
                 return null;
             }
 
             @Override
             public DataSource getNonJtaDataSource() {
-                System.err.println("public DataSource getNonJtaDataSource()");
                 return null;
             }
 
             @Override
             public List<String> getMappingFileNames() {
-                System.err.println("public List<String> getMappingFileNames()");
                 return Collections.emptyList();
             }
 
             @Override
             public List<URL> getJarFileUrls() {
-                System.err.println("public List<URL> getJarFileUrls()");
                 try {
                     return Collections.list(this.getClass()
                             .getClassLoader()
@@ -110,13 +104,11 @@ public class DemoUserStorageProviderFactory implements UserStorageProviderFactor
 
             @Override
             public URL getPersistenceUnitRootUrl() {
-                System.err.println("public URL getPersistenceUnitRootUrl()");
                 return null;
             }
 
             @Override
             public List<String> getManagedClassNames() {
-                System.err.println("public List<String> getManagedClassNames()");
                 List<String> managedClasses = new LinkedList<>();
                 managedClasses.add(User.class.getName());
                 return managedClasses;
@@ -124,48 +116,41 @@ public class DemoUserStorageProviderFactory implements UserStorageProviderFactor
 
             @Override
             public boolean excludeUnlistedClasses() {
-                System.err.println("public boolean excludeUnlistedClasses()");
                 return false;
             }
 
             @Override
             public SharedCacheMode getSharedCacheMode() {
-                System.err.println("public SharedCacheMode getSharedCacheMode()");
                 return SharedCacheMode.UNSPECIFIED;
             }
 
             @Override
             public ValidationMode getValidationMode() {
-                System.err.println("public ValidationMode getValidationMode()");
                 return ValidationMode.AUTO;
             }
 
             @Override
             public Properties getProperties() {
-                System.err.println("public Properties getProperties()");
                 return new Properties();
             }
 
             @Override
             public String getPersistenceXMLSchemaVersion() {
-                System.err.println("public String getPersistenceXMLSchemaVersion()");
                 return "2.1";
             }
 
             @Override
             public ClassLoader getClassLoader() {
-                System.err.println("public ClassLoader getClassLoader()");
-                return Thread.currentThread().getContextClassLoader();
+                return null;
+//                return Thread.currentThread().getContextClassLoader();
             }
 
             @Override
             public void addTransformer(ClassTransformer transformer) {
-                System.err.println("public void addTransformer(ClassTransformer transformer)");
             }
 
             @Override
             public ClassLoader getNewTempClassLoader() {
-                System.err.println("public ClassLoader getNewTempClassLoader()");
                 return null;
             }
         };
@@ -174,7 +159,7 @@ public class DemoUserStorageProviderFactory implements UserStorageProviderFactor
     @Override
     public DemoUserStorageProvider create(KeycloakSession session, ComponentModel model) {
         log.infov("Create new provider {0}", "demo-storage");
-        UserDAO userDAO = new UserDAO(/*entityManagerFactory.createEntityManager()*/ entityManagerFactory.createEntityManager());
+        UserDAO userDAO = new UserDAO(entityManagerFactory.createEntityManager());
         return new DemoUserStorageProvider(session, model, userDAO);
     }
 
--- src/main/java/com/example/keycloakuserstore/dao/UserDAO.java
+++ src/main/java/com/example/keycloakuserstore/dao/UserDAO.java
@@ -1,14 +1,21 @@
 package com.example.keycloakuserstore.dao;
 
 import com.example.keycloakuserstore.models.User;
+import org.hibernate.Transaction;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.FlushModeType;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.UUID;
+import java.util.logging.Logger;
 
 public class UserDAO {
 
     private EntityManager entityManager;
+    Logger logger = Logger.getLogger(UserDAO.class.getName());
 
     public UserDAO(EntityManager entityManager) {
         this.entityManager = entityManager;
@@ -20,9 +27,40 @@ public class UserDAO {
         return query.getResultList();
     }
 
+    public List<User> findAll(int start, int max) {
+        TypedQuery<User> query = entityManager.createNamedQuery("searchForUser", User.class);
+        query.setFirstResult(start);
+        query.setMaxResults(max);
+        query.setParameter("search", "%");
+        List<User> users =  query.getResultList();
+        System.err.println(users);
+        return users;
+    }
+
+    public User getUserByUsername(String username) {
+        logger.info("getUserByUsername(username: " + username + ")");
+        TypedQuery<User> query = entityManager.createNamedQuery("getUserByUsername", User.class);
+        query.setParameter("username", username);
+        return query.getResultList().get(0);
+    }
+
+    public List<User> searchForUserByUsernameOrEmail(String searchString) {
+        logger.info("searchForUserByUsernameOrEmail(searchString: " + searchString + ")");
+        TypedQuery<User> query = entityManager.createNamedQuery("searchForUser", User.class);
+        query.setParameter("search", searchString);
+        return query.getResultList();
+    }
+
     public User createUser(User user) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
         entityManager.persist(user);
+        transaction.commit();
         return user;
+    }
+
+    public void close() {
+        this.entityManager.close();
     }
 
 }
