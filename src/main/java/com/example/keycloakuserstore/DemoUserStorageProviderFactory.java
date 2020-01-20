af980df81394266ca2dfeb9ea75aff0b65dd2d32
Dorian Weidler <dorian.weidler@novatec-gmbh.de>
2020-01-20 11:49:28 +0100

    Database configuration in keycloak

--- src/main/java/com/example/keycloakuserstore/DemoUserStorageProviderFactory.java
+++ src/main/java/com/example/keycloakuserstore/DemoUserStorageProviderFactory.java
@@ -2,22 +2,24 @@ package com.example.keycloakuserstore;
 
 import com.example.keycloakuserstore.dao.UserDAO;
 import com.example.keycloakuserstore.models.User;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.hibernate.jpa.boot.internal.ParsedPersistenceXmlDescriptor;
-import org.hibernate.jpa.boot.internal.PersistenceXmlParser;
-import org.hibernate.jpa.boot.spi.Bootstrap;
-import org.keycloak.Config;
-import org.keycloak.component.ComponentModel;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.storage.UserStorageProviderFactory;
 import lombok.extern.jbosslog.JBossLog;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.keycloak.Config;
+import org.keycloak.common.util.MultivaluedHashMap;
+import org.keycloak.component.ComponentModel;
+import org.keycloak.component.ComponentValidationException;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.models.RealmModel;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderConfigurationBuilder;
+import org.keycloak.storage.UserStorageProviderFactory;
 
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.SharedCacheMode;
 import javax.persistence.ValidationMode;
 import javax.persistence.spi.ClassTransformer;
-import javax.persistence.spi.PersistenceProvider;
 import javax.persistence.spi.PersistenceUnitInfo;
 import javax.persistence.spi.PersistenceUnitTransactionType;
 import javax.sql.DataSource;
@@ -32,31 +34,126 @@ import java.util.*;
 @JBossLog
 public class DemoUserStorageProviderFactory implements UserStorageProviderFactory<DemoUserStorageProvider> {
 
+    public static final int PORT_LIMIT = 65535;
     Map properties;
-    EntityManagerFactory entityManagerFactory;
+    Map<String, EntityManagerFactory> entityManagerFactories = new HashMap<>();
+
+    protected static final List<ProviderConfigProperty> configMetadata;
+
+    public static final String DB_CONNECTION_NAME_KEY = "db:connectionName";
+    public static final String DB_HOST_KEY = "db:host";
+    public static final String DB_DATABASE_KEY = "db:database";
+    public static final String DB_USERNAME_KEY = "db:username";
+    public static final String DB_PASSWORD_KEY = "db:password";
+    public static final String DB_PORT_KEY = "db:port";
+
+    static {
+        configMetadata = ProviderConfigurationBuilder.create()
+                // Connection Name
+                .property().name(DB_CONNECTION_NAME_KEY)
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .label("Connection Name")
+                .defaultValue("")
+                .helpText("Name of the connection, can be chosen individually. Enables connection sharing between providers if the same name is provided. Overrides currently saved connection properties.")
+                .add()
+
+                // Connection Host
+                .property().name(DB_HOST_KEY)
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .label("Database Host")
+                .defaultValue("localhost")
+                .helpText("Host of the connection")
+                .add()
+
+                // Connection Database
+                .property().name(DB_DATABASE_KEY)
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .label("Database Name")
+                .add()
+
+                // DB Username
+                .property().name(DB_USERNAME_KEY)
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .label("Database Username")
+                .defaultValue("user")
+                .add()
+
+                // DB Password
+                .property().name(DB_PASSWORD_KEY)
+                .type(ProviderConfigProperty.PASSWORD)
+                .label("Database Password")
+                .defaultValue("PASSWORD")
+                .add()
+
+                // DB Port
+                .property().name(DB_PORT_KEY)
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .label("Database Port")
+                .defaultValue("3306")
+                .add()
+                .build();
+    }
 
     @Override
-    public void init(Config.Scope config) {
-  
-      // this configuration is pulled from the SPI configuration of this provider in the standalone[-ha] / domain.xml
-      // see setup.cli
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configMetadata;
+    }
+
+    @Override
+    public DemoUserStorageProvider create(KeycloakSession session, ComponentModel model) {
         properties = new HashMap();
-        properties.put("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-        properties.put("hibernate.connection.url", "jdbc:mysql://localhost/jee_test");
-        properties.put("hibernate.connection.username", "root");
-        properties.put("hibernate.connection.password", "root");
-//        properties.put("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
-        properties.put("hibernate.show-sql", "true");
-        properties.put("hibernate.archive.autodetection", "class, hbm");
-        properties.put("hibernate.hbm2ddl.auto", "update");
-        properties.put("hibernate.connection.autocommit", "true");
+        String dbConnectionName = model.getConfig().getFirst("db:connectionName");
+        EntityManagerFactory entityManagerFactory = entityManagerFactories.get(dbConnectionName);
+        if(entityManagerFactory == null) {
+            MultivaluedHashMap<String, String> config = model.getConfig();
+            properties.put("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            properties.put("hibernate.connection.url",
+                    String.format("jdbc:mysql://%s:%s/%s",
+                            config.getFirst(DB_HOST_KEY),
+                            config.getFirst(DB_PORT_KEY),
+                            config.getFirst(DB_DATABASE_KEY)));
+            properties.put("hibernate.connection.username", config.getFirst(DB_USERNAME_KEY));
+            properties.put("hibernate.connection.password", config.getFirst(DB_PASSWORD_KEY));
+            properties.put("hibernate.show-sql", "true");
+            properties.put("hibernate.archive.autodetection", "class, hbm");
+            properties.put("hibernate.hbm2ddl.auto", "update");
+            properties.put("hibernate.connection.autocommit", "true");
 
-        /*entityManagerFactory = Persistence.createEntityManagerFactory("h2userstorage");*/
-        entityManagerFactory = new HibernatePersistenceProvider().createContainerEntityManagerFactory(getPersistenceUnitInfo("h2userstorage"), properties);
-        /*entityManagerFactory = Persistence.createEntityManagerFactory("h2userstorage",properties);*/
+            entityManagerFactory = new HibernatePersistenceProvider().createContainerEntityManagerFactory(getPersistenceUnitInfo("h2userstorage"), properties);
+            entityManagerFactories.put(dbConnectionName, entityManagerFactory);
+        }
+        UserDAO userDAO = new UserDAO(entityManagerFactory.createEntityManager());
+        return new DemoUserStorageProvider(session, model, userDAO);
+    }
 
-      String someProperty = config.get("someProperty");
-      log.infov("Configured {0} with someProperty: {1}", this, someProperty);
+    @Override
+    public void onUpdate(KeycloakSession session, RealmModel realm, ComponentModel oldModel, ComponentModel newModel) {
+        String oldCnName = oldModel.getConfig().getFirst(DB_CONNECTION_NAME_KEY);
+        entityManagerFactories.remove(oldCnName);
+        onCreate(session, realm, newModel);
+    }
+
+    @Override
+    public void validateConfiguration(KeycloakSession session, RealmModel realm, ComponentModel config) throws ComponentValidationException {
+        MultivaluedHashMap<String, String> configMap = config.getConfig();
+        if(StringUtils.isBlank(configMap.getFirst(DB_CONNECTION_NAME_KEY))) {
+            throw new ComponentValidationException("Connection name empty.");
+        }
+        if(StringUtils.isBlank(configMap.getFirst(DB_HOST_KEY))) {
+            throw new ComponentValidationException("Database host empty.");
+        }
+        if(!StringUtils.isNumeric(configMap.getFirst(DB_PORT_KEY)) || Long.parseLong(configMap.getFirst(DB_PORT_KEY)) > PORT_LIMIT) {
+            throw new ComponentValidationException("Invalid port. (Empty or NaN)");
+        }
+        if(StringUtils.isBlank(configMap.getFirst(DB_DATABASE_KEY))) {
+            throw new ComponentValidationException("Database name empty.");
+        }
+        if(StringUtils.isBlank(configMap.getFirst(DB_USERNAME_KEY))) {
+            throw new ComponentValidationException("Database username empty.");
+        }
+        if(StringUtils.isBlank(configMap.getFirst(DB_PASSWORD_KEY))) {
+            throw new ComponentValidationException("Database password empty.");
+        }
     }
 
     private PersistenceUnitInfo getPersistenceUnitInfo(String name) {
@@ -157,14 +254,7 @@ public class DemoUserStorageProviderFactory implements UserStorageProviderFactor
     }
 
     @Override
-    public DemoUserStorageProvider create(KeycloakSession session, ComponentModel model) {
-        log.infov("Create new provider {0}", "demo-storage");
-        UserDAO userDAO = new UserDAO(entityManagerFactory.createEntityManager());
-        return new DemoUserStorageProvider(session, model, userDAO);
-    }
-
-    @Override
     public String getId() {
-        return "demo-user-provider";
+        return "demo-mysql-user-provider";
     }
 }
